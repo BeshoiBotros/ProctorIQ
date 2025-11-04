20ac7e889e849beecf5a41bef0c3b5507fb1487a
Mostafa <mostafa2742002mahmoud@gmail.com>
2025-11-04 19:55:31 +0200

    1- create the question views 2- create the question urls

--- exams/urls.py
+++ exams/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import ExamView
+from .views import *
 
 urlpatterns = [
     path('exams/', ExamView.as_view(), name='exam-list'),
     path('exams/<int:pk>/', ExamView.as_view(), name='exam-detail'),
+
+
+    path('exams/<int:exam_id>/questions/', QuestionView.as_view(), name='question-list'),
+    path('questions/<int:pk>/', QuestionView.as_view(), name='question-detail'),
 ]
--- exams/views.py
+++ exams/views.py
@@ -109,6 +109,104 @@ class ExamView(APIView) :
         exam.delete()
         return Response({"detail": "Exam deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
 
+from .models import Exam, Question
+from .serializers import QuestionSerializer
+from ProctorIQ.utils import IsTeacher, IsStudent
+
+
+class QuestionView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk=None, exam_id=None):
+
+        user = request.user
+
+
+        if user.is_staff:
+            if pk:
+                question = get_object_or_404(Question, pk=pk)
+                serializer = QuestionSerializer(question)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            questions = Question.objects.all()
+            serializer = QuestionSerializer(questions, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+        elif user.role == 'Teacher':
+            if exam_id:
+                exam = get_object_or_404(Exam, id=exam_id, created_by=user)
+                questions = exam.questions.all()
+                serializer = QuestionSerializer(questions, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            if pk:
+                question = get_object_or_404(Question, pk=pk, exam__created_by=user)
+                serializer = QuestionSerializer(question)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response({"detail": "Exam ID is required to view questions."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        elif user.role == 'Student':
+            if not exam_id:
+                return Response({"detail": "Exam ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+            exam = Exam.objects.filter(id=exam_id, attempts__student=user).first()
+            if not exam:
+                return Response({"detail": "You are not assigned to this exam."}, status=status.HTTP_403_FORBIDDEN)
+
+            questions = exam.questions.all()
+            serializer = QuestionSerializer(questions, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response({"detail": "Unauthorized user role."}, status=status.HTTP_403_FORBIDDEN)
+
+    def post(self, request, exam_id=None):
+
+        user = request.user
+
+        if user.role != 'Teacher':
+            return Response({"detail": "Only teachers can create questions."}, status=status.HTTP_403_FORBIDDEN)
+
+        if not exam_id:
+            return Response({"detail": "Exam ID is required to create a question."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        exam = get_object_or_404(Exam, id=exam_id, created_by=user)
+
+        data = request.data.copy()
+        data['exam'] = exam.id
+
+        serializer = QuestionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save(exam=exam)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+
+        user = request.user
+        question = get_object_or_404(Question, pk=pk)
+
+        if user.role == 'Teacher' and question.exam.created_by != user:
+            return Response({"detail": "You can only update questions in your exams."}, status=status.HTTP_403_FORBIDDEN)
+
+        serializer = QuestionSerializer(question, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+
+        user = request.user
+        question = get_object_or_404(Question, pk=pk)
+
+        if user.role == 'Teacher' and question.exam.created_by != user:
+            return Response({"detail": "You can only delete questions in your exams."}, status=status.HTTP_403_FORBIDDEN)
+
+        question.delete()
+        return Response({"detail": "Question deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
 
     
 
