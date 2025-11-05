d292dca25681a7d30628d24fc298d632deffe27b
Mostafa <mostafa2742002mahmoud@gmail.com>
2025-11-05 13:34:13 +0200

    fix dublicate in exams urls

--- exams/urls.py
+++ exams/urls.py
@@ -2,27 +2,26 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # exams
-    path('exams/', ExamView.as_view(), name='exam-list'),
-    path('exams/<int:pk>/', ExamView.as_view(), name='exam-detail'),
+    # Exams
+    path('', ExamView.as_view(), name='exam-list'),
+    path('<int:pk>/', ExamView.as_view(), name='exam-detail'),
 
-    # questions
-    path('exams/<int:exam_id>/questions/', QuestionView.as_view(), name='question-list'),
+    # Questions
+    path('<int:exam_id>/questions/', QuestionView.as_view(), name='question-list'),
     path('questions/<int:pk>/', QuestionView.as_view(), name='question-detail'),
 
     # Choices
     path('questions/<int:question_id>/choices/', ChoiceView.as_view(), name='choice-list'),
     path('choices/<int:pk>/', ChoiceView.as_view(), name='choice-detail'),
 
-    # attempts
+    # Attempts
     path('attempts/', ExamAttemptView.as_view(), name='attempt-list'),
     path('attempts/<int:pk>/', ExamAttemptView.as_view(), name='attempt-detail'),
 
-    # Student Answer
+    # Student Answers
     path('attempts/<int:attempt_id>/answers/', StudentAnswerView.as_view(), name='answer-list'),
     path('answers/<int:pk>/', StudentAnswerView.as_view(), name='answer-detail'),
 
     # Exam Results
-    path('exams/<int:exam_id>/results/', ExamResultsView.as_view(), name='exam-results'),
-
+    path('<int:exam_id>/results/', ExamResultsView.as_view(), name='exam-results'),
 ]
